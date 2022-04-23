170d6f091d538122aeb03c935d064e61f59c1398
Duy-Kun <sherlockduy2001@gmail.com>
2022-04-23 15:59:30 +0700

    add name and email class for input email and email

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,30 +4,30 @@ function ContactPage() {
         // insert your code between the <div></div> below
         <div>
             <div class="alert-success">
-            <span>Message Sent! Thank you for contacting us.</span>
+                <span>Message Sent! Thank you for contacting us.</span>
             </div>
 
             <div class="alert-error">
-            <span>Something went wrong! Please try again.</span>
+                <span></span>
             </div>
 
             <div class="contact-section">
                 <div class="contact-info">
                     <div><i class="fa-solid fa-location-dot"></i>Address, City, Country</div>
-                    <div><i class="fa-solid fa-location-dot"></i>spectre.cine@gmail.com</div>
+                    <div><i class="fa-solid fa-location-dot"></i>contact@gmail.com</div>
                     <div><i class="fa-solid fa-location-dot"></i>+84 905988777</div>
                     <div><i class="fa-solid fa-location-dot"></i>Mon-Fri 08:00 AM to 05:00 PM</div>
-            </div>
+                </div>
             <div class="contact-form">
-            <h2>Contact Us</h2>
+                <h2>Contact Us</h2>
                 <form class="contact" action="" method="post">
-                    <input type="text" name="name" class="text-box" placeholder="Your Name" required/>
-                    <input type="email" name="email" class="text-box" placeholder="Your Email" required/>
+                    <input type="text" name="name" class="text-box name" placeholder="Your Name" required/>
+                    <input type="email" name="email" class="text-box email" placeholder="Your Email" required/>
                     <textarea name="message" rows="5" placeholder="Your Message"required></textarea>
                 </form>
                 <button class="send-btn">Send</button>
             </div>
-    </div>
+        </div>
 
         </div>
     )
